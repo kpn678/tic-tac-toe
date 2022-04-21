c6c1ab932c866b6654b71f24a198d624d7f19353
Kristy Nguyen <knyen008@gmail.com>
2022-04-21 13:22:56 -0600

    Create update game board data function

--- game.js
+++ game.js
@@ -1,32 +1,49 @@
 class Game {
   constructor() {
-    this.board = [b1, b2, b3, b4, b5, b6, b7, b8, b9]
+    this.board = [
+    'b0', 'b1', 'b2',
+    'b3', 'b4', 'b5',
+    'b6', 'b7', 'b8'
+    ];
     this.player1 = new Player(1, 'X', 0, true);
     this.player2 = new Player(2, 'O', 0, false);
+    this.currentPlayer = updateTurn();
     this.turnCounter = 0;
-
   }
-  updateGameBoardData() {
 
+  updateGameBoardData(selectedSpot) {
+    for (var i = 0; i < this.board.length, i++) {
+      if (this.board[i] === selectedSpot) {
+        this.board.splice(i, 1, this.currentPlayer.token)
+      }
+    }
   }
-  
+
   updateTurn() {
+    this.turnCounter++;
     if (this.turnCounter % 2 === 0 && this.player1.isTurn === true) {
-      return this.player2.isTurn === true && this.player1.isTurn === false;
+      this.player2.isTurn === true
+      this.player1.isTurn === false;
+      return player2;
     } else {
-      return this.player1.isTurn === true && this.player2.isTurn === false;
+      this.player1.isTurn === true;
+      this.player2.isTurn === false;
+      return player1;
     }
   }
 
   checkForWin() {
   //if various box combinations have been selected, then win happens
   }
+
   checkForDraw() {
-  //if none of conditions in checkForWinCondition selected
-  //and all boxes selected, then draw happens
+    if (this.turnCounter === 9) {
+      resetGame();
+    }
   }
+
   resetGame(){
-  //once win is over, all the boxes are unselected, losing player
+  //once game is over, all the boxes are unselected, losing player
   //begins
   }
 };
