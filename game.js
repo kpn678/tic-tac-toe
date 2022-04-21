424831e79a44b7e44983093b7afec5410fe440c5
Kristy Nguyen <knyen008@gmail.com>
2022-04-21 11:53:37 -0600

    Create update turn function

--- game.js
+++ game.js
@@ -1,15 +1,24 @@
 class Game {
   constructor() {
     this.board = [b1, b2, b3, b4, b5, b6, b7, b8, b9]
+    this.player1 = new Player(1, 'X', 0, true);
+    this.player2 = new Player(2, 'O', 0, false);
+    this.turnCounter = 0;
+
   }
   updateGameBoardData() {
-    var player1 = new Player(1, 'X');
-    var player2 = new Player(2, 'O');
+
   }
-  updatePlayerTurn() {
-  //if player 1 selects, then player 2 selects and vice versa
+  
+  updateTurn() {
+    if (this.turnCounter % 2 === 0 && this.player1.isTurn === true) {
+      return this.player2.isTurn === true && this.player1.isTurn === false;
+    } else {
+      return this.player1.isTurn === true && this.player2.isTurn === false;
+    }
   }
-  checkForWinCondition() {
+
+  checkForWin() {
   //if various box combinations have been selected, then win happens
   }
   checkForDraw() {
--- player.js
+++ player.js
@@ -3,6 +3,7 @@ class Player {
     this.id = id;
     this.token = token;
     this.wins = 0;
+    this.turn = false;
   }
   increaseWins(wins) {
     this.wins++;
