aaced0ed1763ae591ad4a1c4ff12bdaf96447b36
Kristy Nguyen <knyen008@gmail.com>
2022-04-22 15:32:52 -0600

    Add query selectors and event listeners

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 var game = new Game();
+
+//query selectors
+var box0 = document.querySelector('#b0');
+var box1 = document.querySelector('#b1');
+var box2 = document.querySelector('#b2');
+var box3 = document.querySelector('#b3');
+var box4 = document.querySelector('#b4');
+var box5 = document.querySelector('#b5');
+var box6 = document.querySelector('#b6');
+var box7 = document.querySelector('#b7');
+var box8 = document.querySelector('#b8');
