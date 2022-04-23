ee7a510a582b820b96494d787b93b1ad0c732287
Kristy Nguyen <knyen008@gmail.com>
2022-04-23 11:29:38 -0600

    Refactor choose box fn

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
 var game = new Game();
 
 //query selectors
-game.board[0] = document.querySelector('#b0');
-game.board[1] = document.querySelector('#b1');
-game.board[2] = document.querySelector('#b2');
-game.board[3] = document.querySelector('#b3');
-game.board[4] = document.querySelector('#b4');
-game.board[5] = document.querySelector('#b5');
-game.board[6] = document.querySelector('#b6');
-game.board[7] = document.querySelector('#b7');
-game.board[8] = document.querySelector('#b8');
+box0 = document.querySelector('#b0');
+box1 = document.querySelector('#b1');
+box2 = document.querySelector('#b2');
+box3 = document.querySelector('#b3');
+box4 = document.querySelector('#b4');
+box5 = document.querySelector('#b5');
+box6 = document.querySelector('#b6');
+box7 = document.querySelector('#b7');
+box8 = document.querySelector('#b8');
 var gameBoard = document.querySelector('.game-board');
 var turnHeader = document.querySelector('h2');
 var player1Wins = document.querySelector('#player1-wins');
 var player2Wins = document.querySelector('#player2-wins');
 
-game.board[0].addEventListener('click', chooseBox);
-game.board[1].addEventListener('click', chooseBox);
-game.board[2].addEventListener('click', chooseBox);
-game.board[3].addEventListener('click', chooseBox);
-game.board[4].addEventListener('click', chooseBox);
-game.board[5].addEventListener('click', chooseBox);
-game.board[6].addEventListener('click', chooseBox);
-game.board[7].addEventListener('click', chooseBox);
-game.board[8].addEventListener('click', chooseBox);
+box0.addEventListener('click', chooseBox, {once: true});
+box1.addEventListener('click', chooseBox, {once: true});
+box2.addEventListener('click', chooseBox, {once: true});
+box3.addEventListener('click', chooseBox, {once: true});
+box4.addEventListener('click', chooseBox, {once: true});
+box5.addEventListener('click', chooseBox, {once: true});
+box6.addEventListener('click', chooseBox, {once: true});
+box7.addEventListener('click', chooseBox, {once: true});
+box8.addEventListener('click', chooseBox, {once: true});
 
 function chooseBox() {
   var box = event.target;
   if (game.currentPlayer === game.player1) {
     box.innerText = game.player1.token;
-    box.disabled = true;
   } else {
     box.innerText = game.player2.token;
-    box.disabled = true;
   }
-  game.checkForWin();
+  var boxID = event.target.getAttribute('id');
+  game.updateGameBoard(boxID);
 }
 
 // function updateTurnHeader() {
