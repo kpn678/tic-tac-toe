12cf4f43aa9976b543a5efaf7d1b4d73152aea81
Kristy Nguyen <knyen008@gmail.com>
2022-04-23 10:53:54 -0600

    Refactor check for win and update game board fn

--- game.js
+++ game.js
@@ -1,10 +1,5 @@
 class Game {
   constructor() {
-    this.board = [
-    'b0', 'b1', 'b2',
-    'b3', 'b4', 'b5',
-    'b6', 'b7', 'b8'
-    ];
     this.player1 = new Player(1, 'X');
     this.player2 = new Player(2, 'O');
     this.currentPlayer = this.player1;
@@ -12,42 +7,25 @@ class Game {
     this.gamesPlayed = 0;
   }
 
-  updateGameBoardData(selectedSpot) {
-    if (this.board.includes(selectedSpot)) {
-      for (var i = 0; i < this.board.length; i++) {
-        if (this.board[i] === selectedSpot) {
-          this.board.splice(i, 1, this.currentPlayer.token)
-          this.checkForWin();
-        }
-      }
+  updateGameBoard(selectedSpot) {
+    this.turnCounter++;
+    if (this.currentPlayer === this.player1) {
+      this.player1.choices.push(selectedSpot);
     } else {
-      console.log('Spot taken, try again!');
+      this.player2.choices.push(selectedSpot);
     }
+    this.checkGameStatus();
   }
 
-  checkForWin() {
-    if ((this.board[0] === this.player1.token && this.board[1] === this.player1.token && this.board[2] === this.player1.token) ||
-    (this.board[3] === this.player1.token && this.board[4] === this.player1.token && this.board[5] === this.player1.token) ||
-    (this.board[6] === this.player1.token && this.board[7] === this.player1.token && this.board[8] === this.player1.token) ||
-    (this.board[0] === this.player1.token && this.board[3] === this.player1.token && this.board[6] === this.player1.token) ||
-    (this.board[1] === this.player1.token && this.board[4] === this.player1.token && this.board[7] === this.player1.token) ||
-    (this.board[2] === this.player1.token && this.board[5] === this.player1.token && this.board[8] === this.player1.token) ||
-    (this.board[0] === this.player1.token && this.board[4] === this.player1.token && this.board[8] === this.player1.token) ||
-    (this.board[2] === this.player1.token && this.board[4] === this.player1.token && this.board[6] === this.player1.token)) {
+  checkGameStatus() {
+    if (this.checkWinningCombos(this.player1.choices) === true) {
       console.log('Player 1 Wins!')
       this.player1.increaseWins();
       this.resetGame();
-    } else if ((this.board[0] === this.player2.token && this.board[1] === this.player2.token && this.board[2] === this.player2.token) ||
-    (this.board[3] === this.player2.token && this.board[4] === this.player2.token && this.board[5] === this.player2.token) ||
-    (this.board[6] === this.player2.token && this.board[7] === this.player2.token && this.board[8] === this.player2.token) ||
-    (this.board[0] === this.player2.token && this.board[3] === this.player2.token && this.board[6] === this.player2.token) ||
-    (this.board[1] === this.player2.token && this.board[4] === this.player2.token && this.board[7] === this.player2.token) ||
-    (this.board[2] === this.player2.token && this.board[5] === this.player2.token && this.board[8] === this.player2.token) ||
-    (this.board[0] === this.player2.token && this.board[4] === this.player2.token && this.board[8] === this.player2.token) ||
-    (this.board[2] === this.player2.token && this.board[4] === this.player2.token && this.board[6] === this.player2.token)) {
+    } else if (this.checkWinningCombos(this.player2.choices) === true) {
       console.log('Player 2 Wins!')
       this.player2.increaseWins();
-      resetGame();
+      this.resetGame();
     } else if (this.turnCounter === 9) {
       console.log('This is a draw!')
       this.resetGame();
@@ -56,8 +34,20 @@ class Game {
     }
   }
 
+  checkWinningCombos(playerCombos) {
+    if ((playerCombos.includes('b0') && playerCombos.includes('b1') && playerCombos.includes('b2')) ||
+    (playerCombos.includes('b3') && playerCombos.includes('b4') && playerCombos.includes('b5')) ||
+    (playerCombos.includes('b6') && playerCombos.includes('b7') && playerCombos.includes('b8')) ||
+    (playerCombos.includes('b0') && playerCombos.includes('b3') && playerCombos.includes('b6')) ||
+    (playerCombos.includes('b1') && playerCombos.includes('b4') && playerCombos.includes('b7')) ||
+    (playerCombos.includes('b2') && playerCombos.includes('b5') && playerCombos.includes('b8')) ||
+    (playerCombos.includes('b0') && playerCombos.includes('b4') && playerCombos.includes('b8')) ||
+    (playerCombos.includes('b2') && playerCombos.includes('b4') && playerCombos.includes('b6'))) {
+      return true;
+    }
+  }
+
   updateTurn() {
-    this.turnCounter++;
     if (this.currentPlayer === this.player1) {
       this.currentPlayer = this.player2;
       console.log('Player 2 Turn');
@@ -67,18 +57,13 @@ class Game {
     }
   }
 
-  resetGame(){
+  resetGame() {
     this.gamesPlayed++
     if (this.gamesPlayed % 2 === 0) {
       this.currentPlayer = this.player1;
     } else {
       this.currentPlayer = this.player2;
     }
-    this.board = [
-    'b0', 'b1', 'b2',
-    'b3', 'b4', 'b5',
-    'b6', 'b7', 'b8'
-    ];
     this.turnCounter = 0;
   }
 };
